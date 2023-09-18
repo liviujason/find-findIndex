6e435e91f6ab569effe9e00c7aa254820c194534
Liviu Grozav <grozlivj@gmail.com>
2023-09-17 22:44:13 -0700

    Adding two completed practice files

---
+++ app.js
@@ -0,0 +1,12 @@
+const obj = [{user: 'God'}, {user: 'Jesus'}, {user: 'Holy Spirit'}]
+
+function findUserByUsername(arr, str) {
+   return arr.find(function(val) {
+      return val.str === str;
+   });
+}
+
+
+
+
+
